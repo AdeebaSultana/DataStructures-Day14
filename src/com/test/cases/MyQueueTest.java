e6e65639fcd3d71c311166da64b13164e5c26a0c
AdeebaSultana <adizaan8892@gmail.com>
2022-04-21 14:58:59 +0530

    Added INode Program

---
+++ src/com/test/cases/MyQueueTest.java
@@ -0,0 +1,48 @@
+package com.test.cases;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.linked.list.problems.INode;
+import com.linked.list.problems.MyNode;
+
+public class MyQueueTest {
+
+    @Test
+    public void given3NumbersInEnqueOperation() {
+
+        MyNode<Integer> firstNode = new MyNode<Integer>(56);
+        MyNode<Integer> secondNode = new MyNode<Integer>(30);
+        MyNode<Integer> ThirdNode = new MyNode<Integer>(70);
+        com.test.cases.MyQueue myQueue = new com.test.cases.MyQueue();
+        myQueue.enqueu(firstNode);
+        myQueue.enqueu(secondNode);
+        myQueue.enqueu(ThirdNode);
+        myQueue.printQueue();
+        INode dequeue = myQueue.dequeue();
+
+
+        Assert.assertEquals(firstNode, dequeue);
+
+    }
+
+    @Test
+    public void given3NumbersDequeOperation() {
+
+        MyNode<Integer> firstNode = new MyNode<Integer>(56);
+        MyNode<Integer> secondNode = new MyNode<Integer>(30);
+        MyNode<Integer> ThirdNode = new MyNode<Integer>(70);
+        com.test.cases.MyQueue myQueue = new com.test.cases.MyQueue();
+        myQueue.enqueu(firstNode);
+        myQueue.enqueu(secondNode);
+        myQueue.enqueu(ThirdNode);
+        myQueue.dequeue();
+        myQueue.printQueue();
+        INode pop = myQueue.dequeue();
+
+        Assert.assertEquals(secondNode, pop);
+
+    }
+
+
+}
